3bf7e2fe2177d7d558cbc848adb6619a9f84e367
Joel Lawton <joellawton65@gmail.com>
2019-06-15 18:51:04 +0100

    Added body to main.css, gothic font

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,4 @@
 <style>
-.background{
-    position:absolute;
-    background-color: #1e6a8e;
-    background-image: linear-gradient(to top, #ffffff00, #06171f 100%);
-    width: 100%;
-    height: 100%;
-    z-index: 2;
-    top: 0;
-}
-
 .parallax-layers{
     position: absolute;
     width: 100vw;
@@ -31,23 +21,29 @@
 .layer-3{
   width: 100%;
 }
+
+.star{
+    height: 50px;
+    width: 50px;
+    background-color: white;
+}
+
 </style>
 
 <template>
-    <div class="background" v-on:mousemove="parallax">
+    <div class="background" :class="{'to-purple':$route.path === '/About', 'to-orange':$route.path === '/Work'}" v-on:mousemove="parallax">
         <b-navbar type="dark">
             <b-navbar-brand>
-                <router-link to="/"><img src="images/joelLogo.png" alt="Joel Lawton" width="180"/></router-link>
+                <router-link to="/"><img src="images/joelLogo.png" title="Joel Lawton" alt="Joel Lawton" width="180"/></router-link>
                 <a href="https://twitter.com/JoelLawton3" target="_blank"><i class="fa fa-twitter icon-link"></i></a>
                 <a href="https://www.linkedin.com/in/joel-lawton-6a6b37154/" target="_blank"><i class="fa fa-linkedin icon-link"></i></a>
             </b-navbar-brand>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item to="/"><h4>Home</h4></b-nav-item>
-                <b-nav-item to="/About"><h4>About</h4></b-nav-item>
-                <b-nav-item to="/Work"><h4>Work</h4></b-nav-item>
+            <b-navbar-nav class="ml-auto" >
+                <b-nav-item to="/About" class="mr-5" :class="{'btn-inactive': $route.path !== '/About'}" :disabled="$route.path === '/About'"><h5>About me</h5></b-nav-item>
+                <b-nav-item to="/Work" class="mr-5" :class="{'btn-inactive': $route.path !== '/Work'}" :disabled="$route.path === '/Work'"><h5>My work</h5></b-nav-item>
             </b-navbar-nav>
         </b-navbar>
-        <div class="parallax-layers">
+        <!-- <div class="parallax-layers">
             <img class="layer-1" 
             :style="{'transform':'translateX(' + mouseX/500 + '%) translateY(' + mouseY/500 + '%)'}" src="images/stars.png" />
             <img class="layer-2" 
@@ -55,7 +51,7 @@
             <img class="layer-3" src="images/FogLayer.png"/>
             <img class="layer-4" 
             :style="{'transform':'translateX(' + mouseX/200 + '%) translateY(' + mouseY/200 + '%)'}" src="images/TreeLayer.png"/>
-        </div>
+        </div> -->
         <router-view></router-view>
     </div>
 </template>
@@ -65,10 +61,15 @@ export default {
     data(){
         return{
             mouseX:0,
-            mouseY:0
+            mouseY:0,
         }
     },
+    updated(){
+        
+    },
     methods:{
+        createPointerAnimation:function(){
+        },
         parallax:function(){
             this.mouseX = this.lerp(this.mouseX, event.clientX, 0.1);
             this.mouseY = this.lerp(this.mouseY, event.clientY, 0.1);
@@ -76,9 +77,6 @@ export default {
         lerp:function(start, end, amt){
             return (1-amt) * start + amt * end;
         }
-    },
-    computed:{
-        
     }
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="container fade-up">
-        <h1>About Me</h1>
+        <div class="segment-top">
+            <h2>Introduction</h2>
+            <p>Hey there, I'm Joel Lawton, a games and font-end developer from Manchester, UK.</p>
+        </div>
+        <div class="segment-top row">
+            <div class="col-5">
+                <h2>Experience</h2>
+            </div>
+            <div class="col">
+                <ul class="content-list">
+                    <li class="mb-5">
+                        <div class="">
+                            <p class="m-0">Graduate Developer - Interact</p>
+                            <p class="m-0">May 2018 - present</p>
+                        </div>
+                    </li>
+                    <li class="mb-5">
+                        <div class="">
+                            <p class="m-0">Computer and Video Games - Salford University</p>
+                            <p class="m-0">Sep 2015 - May 2018</p>
+                        </div>
+                    </li>
+                    <li class="mb-5">
+                        <div class="">
+                            <p class="m-0">Interactive Media - Bury College</p>
+                            <p class="m-0">Sep 2013 - Jun 2015</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
