7a03fd22a7cf17a3edfdcdcf3a64a334e028ae9e
Joel Lawton <joellawton65@gmail.com>
2019-06-22 16:12:59 +0100

    Added Unioty game loader

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="container fade-up">
         <h1>My Work</h1>
+        <unity src="games/WebTestBuild/Build/WebTestBuild.json" width="1000" height="600" unityLoader="games/WebTestBuild/Build/UnityLoader.js"></unity>
     </div>
 </template>
 
 <script>
+ import Unity from 'vue-unity-webgl'
 export default {
+    nanme: 'work',
+    components:{
+        Unity
+    }
 
 }
 </script>
-
-<style>
-
-</style>
